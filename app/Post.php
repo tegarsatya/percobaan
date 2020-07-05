9af6cd632ea36e6d8616337e68e77f3d10abaf38
Backend Server <backendserver@gmail.com>
2020-07-05 12:51:26 +0700

    Relasi Tabel database post dan category

--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App;
-
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
